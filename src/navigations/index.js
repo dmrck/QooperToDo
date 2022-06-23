f415b3a05a9689682f4c5b8e247f5fc67390dfdd
TURKCELL\USERNAME <email@turkcell.com.tr>
2022-06-24 00:53:02 +0300

    feat: install firebase/auth and check sign in on initial

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,6 +1,7 @@
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
+import auth from '@react-native-firebase/auth';
 
 import SingInScreen from '../screens/sing-in';
 import HomeScreen from '../screens/home';
@@ -8,9 +9,31 @@ import HomeScreen from '../screens/home';
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
+  const [initialScreen, setInitialScreen] = useState('');
+
+  const onAuthStateChanged = user => {
+    if (user) {
+      //getUserData and navigate Home
+    } else {
+      setInitialScreen('SingIn');
+    }
+  };
+
+  useEffect(() => {
+    const userControl = auth().onAuthStateChanged(onAuthStateChanged);
+
+    return userControl;
+  }, []);
+
+  if (initialScreen == '') {
+    return null;
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Navigator
+        initialRouteName={initialScreen}
+        screenOptions={{headerShown: false}}>
         <Stack.Screen name="SingIn" component={SingInScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
